cae32781be9e1dc9fa1cc2877156044c5853e675
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2024-07-16 00:11:25 +0200

    Made switch persistent.

--- popup/popup.js
+++ popup/popup.js
@@ -1,6 +1,17 @@
 function popup() {
   console.info("DOM fully loaded and parsed. Beginning execution of script \"popup.js\".");
-  // main code execution here
+
+  const checkbox = document.querySelector(".satb-switch input");
+  chrome.storage.sync.get(["isActive"], function(result) {
+    if (result.isActive !== undefined) {
+      checkbox.checked = result.isActive;
+    }
+  });
+  checkbox.addEventListener("change", function() {
+    chrome.storage.sync.set({ isActive: checkbox.checked }, function() {
+      console.log("The value is set to " + checkbox.checked);
+    });
+  });
 }
 
 if (document.readyState === "loading") {
