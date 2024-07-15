6b5b829d3a77db0f21a52d993e62222cd58cfc45
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2024-07-16 01:28:39 +0200

    Made extension work a little bit better for playlists, but it's still not perfect.

--- content-scripts/satb.js
+++ content-scripts/satb.js
@@ -6,11 +6,23 @@ function startAtTheBeginning() {
       const searchParams = new URLSearchParams(window.location.search);
       const v = searchParams.get("v");
       const t = searchParams.get("t");
+      const list = searchParams.get("list");
+      var listParam = "";
+      if (list != null) {
+        listParam = "&list=" + list;
+      }
       if (t == null) {
-        // No t parameter found. Do nothing.
+        if (listParam != "") {
+          window.open("https://www.youtube.com/watch?v=" + v + listParam + "&t=0s", "_self");
+        }
       }
       else if (t != "0s") {
-        window.open("https://www.youtube.com/watch?v=" + v + "&t=0s", "_self");
+        if (listParam != "") {
+          window.open("https://www.youtube.com/watch?v=" + v + listParam + "&t=0s", "_self");
+        }
+        else {
+          window.open("https://www.youtube.com/watch?v=" + v + "&t=0s", "_self");
+        }
       }
     }
   });
