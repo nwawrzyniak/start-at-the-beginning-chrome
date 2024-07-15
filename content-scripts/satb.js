5d788611fad15b878814506682ccf150a098dc26
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2024-07-15 21:44:00 +0200

    Progress

--- content-scripts/satb.js
+++ content-scripts/satb.js
@@ -1,6 +1,14 @@
 function startAtTheBeginning() {
   console.info("DOM fully loaded and parsed. Beginning execution of script \"satb.js\".");
-  // main code execution here
+  const searchParams = new URLSearchParams(window.location.search);
+  const v = searchParams.get("v");
+  const t = searchParams.get("t");
+  if (t == null) {
+    // No t parameter found. Do nothing.
+  }
+  else if (t != "0s") {
+    window.open("https://www.youtube.com/watch?v=" + v + "&t=0s","_self");
+  }
 }
 
 if (document.readyState === "loading") {
