621df5b359c4a3733865359f39fb3d312ca68cb0
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2024-07-16 19:03:44 +0200

    Improved behaviour for other pages besides '/watch'. Implemented MutationObserver to detect URL changes without a full page load.

--- content-scripts/satb.js
+++ content-scripts/satb.js
@@ -1,29 +1,51 @@
+function resetVideo() {
+  const searchParams = new URLSearchParams(window.location.search);
+  const v = searchParams.get("v");
+  const t = searchParams.get("t");
+  const list = searchParams.get("list");
+
+  if (v && (!t || t != "0s")) {
+    let newUrl = "https://www.youtube.com/watch?v=" + v + "&t=0s";
+    if (list) {
+      newUrl += "&list=" + list;
+    }
+    window.open(newUrl, "_self");
+  }
+}
+
 function startAtTheBeginning() {
   console.info("DOM fully loaded and parsed. Beginning execution of script \"satb.js\".");
 
   chrome.storage.sync.get(["isActive"], function(result) {
     if (result.isActive) {
-      const searchParams = new URLSearchParams(window.location.search);
-      const v = searchParams.get("v");
-      const t = searchParams.get("t");
-      const list = searchParams.get("list");
-      var listParam = "";
-      if (list != null) {
-        listParam = "&list=" + list;
-      }
-      if (t == null) {
-        if (listParam != "") {
-          window.open("https://www.youtube.com/watch?v=" + v + listParam + "&t=0s", "_self");
+      let currentVideoId = null;
+
+      function checkAndResetVideo() {
+        const searchParams = new URLSearchParams(window.location.search);
+        const v = searchParams.get("v");
+
+        if (v && v !== currentVideoId) {
+          currentVideoId = v;
+          resetVideo();
         }
       }
-      else if (t != "0s") {
-        if (listParam != "") {
-          window.open("https://www.youtube.com/watch?v=" + v + listParam + "&t=0s", "_self");
-        }
-        else {
-          window.open("https://www.youtube.com/watch?v=" + v + "&t=0s", "_self");
-        }
-      }
+
+      checkAndResetVideo();
+
+      // Create a MutationObserver to detect changes in the DOM
+      const observer = new MutationObserver(function(mutations) {
+        mutations.forEach(function(mutation) {
+          if (mutation.type === "childList" && mutation.addedNodes.length > 0) {
+            checkAndResetVideo();
+          }
+        });
+      });
+
+      // Start observing the entire document for changes
+      observer.observe(document, {
+        childList: true,
+        subtree: true
+      });
     }
   });
 }
