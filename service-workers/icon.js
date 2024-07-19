9b35b97011aa45f40fd04d5bad415e7c91620216
Nico Wawrzyniak <nico.wawrzyniak@live.de>
2024-07-19 04:36:18 +0200

    Icon now reflects whether or not the extension is active.

---
+++ service-workers/icon.js
@@ -0,0 +1,19 @@
+function updateIcon() {
+  chrome.storage.sync.get(["isActive"], function (result) {
+    if (result.isActive === undefined) {
+      chrome.storage.sync.set({ isActive: true }, function () {
+        console.log("satb: isActive was undefined, set to true by default.");
+      });
+    }
+  });
+
+  chrome.storage.sync.get(["isActive"], function (result) {
+    if (result.isActive) {
+      chrome.action.setIcon({ path: "/graphics/nav_bar.png" });
+    } else {
+      chrome.action.setIcon({ path: "/graphics/nav_bar_inactive.png" });
+    }
+  });
+}
+
+updateIcon();
